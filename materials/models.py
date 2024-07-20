10e0e7bbcb0046912cc4cbe6e4d344fc7377baf0
Evgeny Egorov <EAEgorov42@gmail.com>
2024-07-20 20:16:27 +0500

    DRF - выполнил задание 1 и 2

--- materials/models.py
+++ materials/models.py
@@ -4,9 +4,9 @@ NULLABLE = {"blank": True, "null": True}
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=120, verbose_name="наименование")
-    slug = models.CharField(max_length=120, verbose_name="слаг", unique=True)
-    description = models.TextField(max_length=255, verbose_name="описание", **NULLABLE)
+    name = models.CharField(max_length=120, verbose_name="наименование курса")
+    slug = models.CharField(max_length=120, verbose_name="имя слаг", unique=True)
+    description = models.TextField(verbose_name="описание курса", **NULLABLE)
     image = models.ImageField(
         upload_to="media/course_image/", verbose_name="изображение", **NULLABLE
     )
@@ -22,12 +22,12 @@ class Course(models.Model):
 
 
 class Lesson(models.Model):
-    name = models.CharField(max_length=120, verbose_name="наименование")
-    slug = models.CharField(max_length=120, verbose_name="слаг", unique=True)
-    description = models.TextField(max_length=255, verbose_name="описание", **NULLABLE)
+    name = models.CharField(max_length=120, verbose_name="наименование урока")
+    slug = models.CharField(max_length=120, verbose_name="имя слаг", unique=True)
+    description = models.TextField(verbose_name="описание урока", **NULLABLE)
     image = models.ImageField(upload_to="media/lesson_image/", verbose_name="изображение", **NULLABLE)
     link_video = models.FileField(upload_to="media/lesson_video", verbose_name="видео", **NULLABLE)
-    name_course = models.ForeignKey(Course, verbose_name="курс", related_name="урок", on_delete=models.CASCADE,
+    name_course = models.ForeignKey(Course, verbose_name="название курса", related_name="урок", on_delete=models.CASCADE,
                                     **NULLABLE)
 
     def __str__(self):
