36bd8eee36d74e0ac04bcb7a1041126eb6a8472e
Evgeny Egorov <EAEgorov42@gmail.com>
2024-07-20 18:13:52 +0500

    DRF - выполнил задание 1 и 2

--- config/settings.py
+++ config/settings.py
@@ -115,9 +115,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = os.getenv('TIME_ZONE')
 
 USE_I18N = True
 
@@ -135,5 +135,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
+FIXTURES_ROOT = os.path.join(BASE_DIR / 'fixtures')
 
 AUTH_USER_MODEL = 'users.User'
--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from materials.models import Course, Lesson
+
+
+@admin.register(Course)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Lesson)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', )
+    list_filter = ('name',)
+    search_fields = ('name', )
